357b3fbd6bb89dc50ff5b4263e0c024730bed62f
kristal1ik <korobovtsevaolga@yandex.ru>
2024-09-30 10:09:29 +0300

    ex.1 factorial is changed

--- RK1/1.py
+++ RK1/1.py
@@ -7,8 +7,8 @@ n = 0
 
 while abs(prev_num - current_num) > eps or n == 0:
     prev_num = current_num
-    factorial = 2 * n + 1
-    for i in range(2, n+1):
+    factorial = 1
+    for i in range(2, 2 * n + 1):
         factorial *= i
     current_num = ((-1) ** (n - 1)) * ((x * (2 * n + 1)) / factorial)
     summ += current_num
