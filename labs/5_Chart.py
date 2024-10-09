e6be27c74a1083dc7b7f7fbbf336857de09fc90f
kristal1ik <korobovtsevaolga@yandex.ru>
2024-10-10 00:37:36 +0300

    fixed(ex. 5, part 1): new strange conditions

--- labs/5_Chart.py
+++ labs/5_Chart.py
@@ -1,75 +1,87 @@
 import math
+# Входные данные
+start = float(input("Введите начальное значение аргумента: "))
+finish = float(input("Введите конечное значение аргумента: "))
+step = float(input("Введите шаг разбиения отрезка: "))
+width = int(input("Введите ширину графика: "))
+# Инициализация переменных
+table = ""  # Таблица для вывода значений функции
 
-# g0 = float(input("Введите начальное значение: "))
-# gn = float(input("Введите конечное значение: "))
-# step = float(input("Введите шаг: "))
-g0 = -20
-gn = 5
-step = 1
-a2_count = 0
-a3_lst = []
-g0_lst = []
+
+# Заголовки таблицы
 header1 = "g"
 header2 = "a1"
 header3 = "a2"
 header4 = "a3"
-table = ""
-print("-" * 60)
-print(f"|{header1:^14}|{header2:^14}|{header3:^14}|{header4:^14}|")
-print("-" * 60)
-for i in range(g0, gn + 1, step):
-    g0 = i / 10
-    g0_lst.append(g0)
-    a1 = g0 ** 3 + 6.1 * g0 ** 2 - 35.4 * g0 - 25.7
-    a2 = g0 ** 2 - math.cos(math.pi * g0)
-    a3 = math.sqrt((a1 ** 2) + (a2 ** 2))
-    a3_lst.append(a3)
-    if a2 > 0:
-        a2_count += 1
-    table += f"|{g0:^14.5g}|{a1:^14.5g}|{a2:^14.5g}|{a3:^14.5g}|\n"
+a3_min = math.inf
+a3_max = -math.inf
+if start < finish:
+    # Вычисления
+    for i in range(int((finish - start) / step) + 1):
+        x = start + i * step  # Добавляем к значению x шаг
+        if x > finish:
+            break
+        else:
+            a1 = x ** 3 + 6.1 * x ** 2 - 35.4 * x - 25.7
+            a2 = x ** 2 - math.cos(math.pi * x)
+            a3 = math.sqrt((a1 ** 2) + (a2 ** 2))
+            if a3_min > a3:
+                a3_min = a3
+            if a3_max < a3:
+                a3_max = a3
+            table += f"|{x:^14.5g}|{a1:^14.5g}|{a2:^14.5g}|{a3:^14.5g}|\n"
 
-print(table[:-1])
-print("-" * 60)
-print("Количество положительных значений a2(g) =", a2_count)
+    # Вывод таблицы значений
+    print("-" * 60)
+    print(f"|{header1:^14}|{header2:^14}|{header3:^14}|{header4:^14}|")
+    print("-" * 60)
+    print(table[:-1])
+    print("-" * 60)
 
-dial_length = 12
-max_a3_value = max(a3_lst)
-min_a3_value = min(a3_lst)
-max_a3_value_difference = (max_a3_value - min_a3_value)
-ratio = max_a3_value_difference / 80
-iteration = 0
-serifs = int(input("Введите количество засечек (от 4 до 8): "))
-serifs_flag = True if 4 <= serifs <= 8 else False
-while not(serifs_flag):
-    print("Некорректное значение!")
-    serifs = int(input("Введите количество засечек (от 4 до 8): "))
-    serifs_flag = True if 4 <= serifs <= 8 else False
+    # Вычисления для графика
+    while True:  # Проверка на входное число
+        k = int(input("Введите количество засечек на оси ординат от 4 до 8): "))
+        if 4 <= k <= 8:
+            break
+        else:
+            print('Количество засечек должно быть от 4 до 8!')
 
+    free_space = width - len(f"{start:.5g}")  # Место для засечек
 
-label = "График функции a2 = g^2 - cos(π*g)"
-print(dial_length * ' ' + label)
-dist = 82 // serifs
-for i in range(dial_length, dial_length + 82 + 1):
-    if i % dist == 0:
-        print("{0:>{1}}".format('s', i), end='')
+    # Вывод засечек
+    print(" " * 14 + f"{a3_min:.5g}", end="")
 
-print("{0:>{1}}\n {2:>{1}}>".format('y', dial_length + 82, 82 * "-"))
-x_pos = abs(int((- min_a3_value) / ratio))
+    for i in range(1, k):
+        current_y = a3_min + abs(a3_max - a3_min) / (k - 1) * i
+        current_space = free_space // (k - i)
+        free_space -= current_space
 
+        print(" " * (current_space - len(f"{current_y:.5g}") - 1), f"{current_y:.5g}", end="")
 
+    print('\n')
 
-while iteration < len(a3_lst):
-    y1_cur_value = a3_lst[iteration]
-    cur_y1_value_and_min_difference = (y1_cur_value - min_a3_value) + \
-                                      (y1_cur_value == min_a3_value) * \
-                                      ((max_a3_value == min_a3_value))
-    pos_of_y = int(cur_y1_value_and_min_difference * 80 / \
-                   max_a3_value_difference)
+    one = abs(a3_min - a3_max) / width  # Место для 1 символа
+    zero = int(abs(a3_min) // one)  # Столбец для x = 0
 
-    print("{1:^{0}.6g}".format(dial_length, g0_lst[iteration]), end='')
-    print('|' + pos_of_y * ' ' + '*')
-    x_pos = 0
-    iteration += 1
+    # Построение графика
+    for i in range(int((finish - start) / step) + 1):
+        x = start + i * step  # Добавляем к значению x шаг
+        a1 = x ** 3 + 6.1 * x ** 2 - 35.4 * x - 25.7
+        a2 = x ** 2 - math.cos(math.pi * x)
+        a3 = math.sqrt((a1 ** 2) + (a2 ** 2))
 
-print((dial_length + x_pos) * ' ' + '|\n',
-      (dial_length + x_pos - 3) * ' ' + 'x V')
+        current_x = int(abs(a3_min - a3) // one)  # Текущая позиция
+
+        if current_x == width:
+            current_x -= 1
+
+        # вывод строки графика
+        print(f"{x:<12.5g} |", end="")
+        if zero < current_x and a3_min <= 0 <= a3_max:
+            print(" " * zero + "|" + " " * (current_x - zero - 1) + "*" + " " * (width - current_x - 1))
+        elif zero > current_x and a3_min <= 0 <= a3_max:
+            print(" " * current_x + "*" + " " * (zero - current_x - 1) + "|" + " " * (width - zero - 1))
+        else:
+            print(" " * current_x + "*" + " " * (width - current_x - 1))
+else:
+    print("Некорректные значения!")
