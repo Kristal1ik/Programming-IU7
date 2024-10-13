972fc38528ee961e0710b3cd3e47d595b83bed6d
kristal1ik <korobovtsevaolga@yandex.ru>
2024-10-13 23:00:17 +0300

    ┌∩┐(◣_◢)┌∩┐ add(ex. 5): i hate a.d. ヽ( `д´*)ノ

--- labs/5_Test.py
+++ labs/5_Test.py
@@ -1,26 +1,20 @@
 import math
 
-# g0 = float(input("Введите начальное значение: "))
-# gn = float(input("Введите конечное значение: "))
-# step = float(input("Введите шаг: "))
-g0 = float(input())
-gn = float(input())
-step = float(input())
-a2_count = 0
-iteration = 0
+g0 = float(input("Введите начальное значение: "))
+gn = float(input("Введите конечное значение: "))
+step = float(input("Введите шаг: "))
+eps = 0.00001
 dial_length = 12
 max_value = 2
 min_value = 0
 max_a3_value_difference = (max_value - min_value)
 width = 80
 ratio = max_a3_value_difference / width
-serifs = 2
-label = "График функции y = cos(x) "
+label = "График функции y = cos(x) + 1"
 print(dial_length * ' ' + label)
-dist = width // serifs
 print("{1:>{0}.6g}".format(dial_length, min_value), end='')
 print("{1:>{0}.6g}".format(width, max_value), end='')
-print("{0:>{1}}\n {2:>{1}}>".format('y', dial_length + 82, 82 * "-"))
+print("{0:>{1}}\n {2:>{1}}>".format('y', dial_length + width, width * "-"))
 x_pos = abs(int((- min_value) / ratio))
 for i in range(int((gn - g0) / step) + 1):
     x_current = g0 + i * step
@@ -33,7 +27,5 @@ for i in range(int((gn - g0) / step) + 1):
     print("{1:^{0}.6g}".format(dial_length, x_current), end='')
     print('|' + pos_of_y * ' ' + '*')
     x_pos = 0
-    iteration += 1
-
 print((dial_length + x_pos) * ' ' + '|\n',
       (dial_length + x_pos - 3) * ' ' + 'x V')
