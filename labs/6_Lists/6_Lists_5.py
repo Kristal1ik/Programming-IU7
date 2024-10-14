327aa11921ce3e8ba6bc6be337b1c01469ddd8aa
kristal1ik <korobovtsevaolga@yandex.ru>
2024-10-14 08:16:56 +0300

    add(ex. 6): 5

---
+++ labs/6_Lists/6_Lists_5.py
@@ -0,0 +1,25 @@
+#  Пункт 5 (Поменять местами последний четный и минимальный положительный элементы)
+lst = []
+lst_extremes = []
+n = int(input("Введите размер списка: "))
+while n < 1:
+    print("Некорректное значение! Чило должно быть > 0.")
+    n = int(input("Введите размер списка: "))
+for i in range(1, n + 1):
+    number = int(input(f"Введите {i} элемент списка: "))
+    lst.append(number)
+print("-" * 45)
+last_even = 0
+last_even_index = 0
+min_pos = max(lst)
+min_pos_index = 0
+for i in range(len(lst)):
+    if 0 < lst[i] < min_pos:
+        min_pos = lst[i]
+        print(min_pos)
+        min_pos_index = i
+    if lst[i] % 2 == 0:
+        last_even = lst[i]
+        last_even_index = i
+lst[min_pos_index], lst[last_even_index] = lst[last_even_index], lst[min_pos_index]
+print(lst)
