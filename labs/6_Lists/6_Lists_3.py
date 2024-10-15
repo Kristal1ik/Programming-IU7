d48e95df2aeb2d5611b9aae13ae2823c15c08362
kristal1ik <korobovtsevaolga@yandex.ru>
2024-10-15 12:41:27 +0300

    add(ex. 6, 3): without an additional list

--- labs/6_Lists/6_Lists_3.py
+++ labs/6_Lists/6_Lists_3.py
@@ -1,6 +1,4 @@
-#  Пункт 3
 lst = []
-lst_extremes = []
 n = int(input("Введите размер списка: "))
 while n < 1:
     print("Некорректное значение! Чило должно быть > 0.")
@@ -9,12 +7,18 @@ for i in range(1, n + 1):
     number = int(input(f"Введите {i} элемент списка: "))
     lst.append(number)
 print("-" * 45)
+while True:
+    k = int(input("Введите номер экстремума: "))
+    if not 1 <= k < n:
+        print(f"Номер экстремума должен быть от больше нуля")
+    else:
+        break
+occurrences = 0
 for i in range(1, n - 1):
-    if (lst[i] > lst[i - 1] and lst[i] > lst[i + 1]) or (lst[i] < lst[i - 1] and lst[i] < lst[i + 1]):
-        lst_extremes.append(lst[i])
-k = int(input("Введите номер экстремума в списке (нумерация с 1): "))
-while k > len(lst_extremes):
-    print(f"Введено число, большее искомого количества точек экстремума, оно равно {len(lst_extremes)}!")
-    k = int(input("Введите номер экстремума в списке (нумерация с 1): "))
-print("-" * 45)
-print(f"k-тый экстремум = {lst_extremes[k - 1]}")
+    if lst[i - 1] < lst[i] > lst[i + 1] or lst[i - 1] > lst[i] < lst[i + 1]:
+        occurrences += 1
+        if occurrences == k:
+            print(f"K-й экстремум: {lst[i]} на индексе {i}")
+            break
+else:
+    print("В списке меньше K экстремумов:(")
