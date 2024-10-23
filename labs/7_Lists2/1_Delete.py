f21647845cb34b976bb3860a290796cba3bfda04
kristal1ik <korobovtsevaolga@yandex.ru>
2024-10-24 00:23:56 +0300

    add(ex. 7, 1): removing odd elements

---
+++ labs/7_Lists2/1_Delete.py
@@ -0,0 +1,29 @@
+"""
+
+Выполнила: Коробовцева Ольга
+Группа: ИУ7-11Б
+
+Пункт 1
+Удаление нечетных элементов из списка
+
+"""
+
+lst = []
+n = int(input("Введите размер списка: "))
+while n < 1:
+    print("Некорректное значение! Чило должно быть > 0.")
+    n = int(input("Введите размер списка: "))
+for i in range(1, n + 1):
+    number = int(input(f"Введите {i} элемент списка: "))
+    lst.append(number)
+print("-" * 45)
+
+index = 0
+for i in range(len(lst)):
+    if lst[i] % 2 != 0:
+        lst[index] = lst[i]
+        index += 1
+lst = lst[:index]
+print(f"Список с удаленными нечетными элементами: ")
+for i in range(index):
+    print(f"{i+1} элемент = {lst[i]}")
