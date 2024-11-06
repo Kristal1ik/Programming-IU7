2ed08f43b83ccb90f125010913b73312f53efacb
kristal1ik <korobovtsevaolga@yandex.ru>
2024-11-06 23:44:17 +0300

    add(ex. 9, 1): matrix, perfect squares

---
+++ labs/9_Matrix2/1_Perfect_square.py
@@ -0,0 +1,40 @@
+"""
+
+Выполнила: Коробовцева Ольга
+Группа: ИУ7-11Б
+
+Пункт 1
+Определить количество полных квадратов в каждой строке матрицы.Записать значения в массив S
+
+"""
+
+import math
+
+n = int(input("Введите длину списков: A, B: "))
+a = []
+b = []
+mx = [[0] * n for _ in range(n)]
+s = []
+width = 5
+while n <= 0:
+    print("Длина должна быть больше 0!")
+    n = int(input("Введите длину списков: A, B"))
+print("Начнинайте вводить элементы списка A: ")
+for i in range(n):
+    a.append(int(input()))
+print("Начнинайте вводить элементы списка B: ")
+for i in range(n):
+    b.append(int(input()))
+for i in range(n):
+    for j in range(n):
+        item = a[i] * b[j]
+        mx[i][j] = item
+        if item == math.sqrt(item) ** 2:
+            s.append(item)
+
+for i in range(n):
+    row_str = "".join(str(mx[i][j]).ljust(width) for j in range(n))
+    if i < n:
+        row_str += "| " + str(s[i]).rjust(width)
+
+    print(row_str)
