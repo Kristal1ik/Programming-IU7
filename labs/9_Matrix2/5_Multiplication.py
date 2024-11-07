5ef34ab78bc5c1ff283edbdd448517699558627f
kristal1ik <korobovtsevaolga@yandex.ru>
2024-11-07 22:39:13 +0300

    feat(ex. 9, 5): correct multiplication

--- labs/9_Matrix2/5_Multiplication.py
+++ labs/9_Matrix2/5_Multiplication.py
@@ -16,18 +16,22 @@ def matrix_output(matrix, name):
         print(formatted_row)
 
 
-n = int(input("Введите длину матриц A и B: "))
+n = int(input("Введите длину матрицы A: "))
 while n <= 0:
-    print("Длина матриц должна быть больше 0!")
-    n = int(input("Введите длину матриц A и B: "))
-m = int(input("Введите ширину матриц A и B: "))
+    print("Длина матрицы должна быть больше 0!")
+    n = int(input("Введите длину матрицы A: "))
+m = int(input("Введите ширину матрицы A (она равна длине матрицы B): "))
 while m <= 0:
-    print("Ширина матриц должна быть больше 0!")
-    m = int(input("Введите ширину матриц A и B: "))
+    print("Значение должно быть больше 0!")
+    m = int(input("Введите ширину матрицы A (она равна длине матрицы B): "))
+k = int(input("Введите ширину матрицы B: "))
+while k <= 0:
+    print("Щирина матрицы должна быть больше 0!")
+    k = int(input("Введите ширину матрицы B: "))
 
 a = []
 b = []
-c = [[0] * n for _ in range(m)]
+c = [[0] * k for _ in range(n)]
 
 print("-" * 20 + "Заполнение матрицы A" + "-" * 20)
 for i in range(n):
@@ -35,15 +39,18 @@ for i in range(n):
     for j in range(m):
         row.append(int(input(f"Введите {j + 1} элемент {i + 1} строки: ")))
     a.append(row)
+
 print("-" * 20 + "Заполнение матрицы B" + "-" * 20)
-for i in range(n):
+for i in range(m):
     row = []
-    for j in range(m):
+    for j in range(k):
         row.append(int(input(f"Введите {j + 1} элемент {i + 1} строки: ")))
     b.append(row)
-for i in range(n):
-    for j in range(m):
-        c[i][j] = a[i][j] * b[i][j]
+
+for i in range(n):  # по столбцам a
+    for j in range(k):  # по строкам b
+        for p in range(m):
+            c[i][j] += a[i][p] * b[p][j]
 
 matrix_output(a, "A")
 matrix_output(b, "B")
