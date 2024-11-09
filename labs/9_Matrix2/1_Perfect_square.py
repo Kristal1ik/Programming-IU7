3934f9e78c9683118e9c1df697d6ecf34d6e9aab
kristal1ik <korobovtsevaolga@yandex.ru>
2024-11-09 13:05:36 +0300

    fix(ex. 9, 1): correct definition of a perfect square

--- labs/9_Matrix2/1_Perfect_square.py
+++ labs/9_Matrix2/1_Perfect_square.py
@@ -4,7 +4,7 @@
 Группа: ИУ7-11Б
 
 Пункт 1
-Определить количество полных квадратов в каждой строке матрицы.Записать значения в массив S
+Определить количество полных квадратов в каждой строке матрицы. Записать значения в массив S
 
 """
 
@@ -14,7 +14,7 @@ n = int(input("Введите длину списков: A, B: "))
 a = []
 b = []
 mx = [[0] * n for _ in range(n)]
-s = []
+s = [0] * n
 width = 5
 while n <= 0:
     print("Длина должна быть больше 0!")
@@ -29,12 +29,11 @@ for i in range(n):
     for j in range(n):
         item = a[i] * b[j]
         mx[i][j] = item
-        if item == math.sqrt(item) ** 2:
-            s.append(item)
-
+        if item == math.isqrt(item) ** 2:  # isqrt() -- корень без десятичной части
+            print(item)
+            s[i] += 1
 for i in range(n):
     row_str = "".join(str(mx[i][j]).ljust(width) for j in range(n))
     if i < n:
         row_str += "| " + str(s[i]).rjust(width)
-
     print(row_str)
