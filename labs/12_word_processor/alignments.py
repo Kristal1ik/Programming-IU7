887677859bb84763a31e8a113c315aa7cb103d3e
kristal1ik <korobovtsevaolga@yandex.ru>
2024-12-11 18:48:41 +0300

    fix(ex. 12): alignments, calculating

--- labs/12_word_processor/alignments.py
+++ labs/12_word_processor/alignments.py
@@ -1,16 +1,38 @@
 def left(text):
     for i in range(len(text)):
-        while text[i][0] == " ":
-            text[i] = text[i][1:]
+        new_line = ""
+        in_word = False
+
+        for char in text[i]:
+            if char != " ":
+                new_line += char
+                in_word = True
+            elif in_word:
+                new_line += " "
+                in_word = False
+
+        text[i] = new_line
     return text
 
 
 def right(text, length):
     for i in range(len(text)):
-        while len(text[i]) != length:
-            text[i] = " " + text[i]
-    return text
+        new_line = ""
+        in_word = False
 
+        for char in text[i]:
+            if char != " ":
+                new_line += char
+                in_word = True
+            elif in_word:
+                new_line += " "
+                in_word = False
+
+        text[i] = new_line
+        while len(text[i]) < length:
+            text[i] = " " + text[i]
+
+    return text
 
 def width(text, mx_len):
     for i in range(len(text)):
@@ -24,8 +46,13 @@ def width(text, mx_len):
         spaces_counter = text[i].count(' ')
         spaces_words = len(text[i]) - spaces_counter
         needed_spaces = mx_len - spaces_words
-        counter_needed_spaces = needed_spaces // spaces_counter
-        counter_needed_spaces_extra = needed_spaces % spaces_counter
+        counter_needed_spaces_extra = 0
+        if not(spaces_counter == 0):
+            counter_needed_spaces = needed_spaces // spaces_counter
+            counter_needed_spaces_extra = needed_spaces % spaces_counter
+        else:
+            counter_needed_spaces = needed_spaces
+            counter_needed_spaces = needed_spaces
         text[i] = text[i].replace(' ', ' ' * counter_needed_spaces)
         text[i] = text[i].replace(' ' * counter_needed_spaces, ' ' * (counter_needed_spaces + 1),
                                   counter_needed_spaces_extra)
--- labs/12_word_processor/polish_notation.py
+++ labs/12_word_processor/polish_notation.py
@@ -7,6 +7,9 @@ def precedence(op):
 
 
 def apply_operator(operators, values):
+    if len(values) < 2:
+        raise ValueError("Ошибка: недостаточно значений для выполнения операции.")
+
     right = values.pop()
     left = values.pop()
     operator = operators.pop()
@@ -37,7 +40,7 @@ def evaluate_expression(expression):
             values.append(num)
             continue
 
-        while (operators and precedence(operators[-1]) >= precedence(char)):
+        while operators and precedence(operators[-1]) >= precedence(char):
             apply_operator(operators, values)
 
         operators.append(char)
@@ -48,18 +51,28 @@ def evaluate_expression(expression):
 
     if not values:
         raise ValueError("Ошибка: выражение не содержит чисел.")
-
     return values[0]
 
 
+def remove_empty_lines(lines):
+    return [line for line in lines if line.strip() != '']
+
+
+def remove_lines_with_text(lines, text_to_remove):
+    return [line for line in lines if text_to_remove not in line]
+
+
 def extract_and_evaluate(lines):
+    current_expression = ''
+    lines = remove_empty_lines(lines)
     for index in range(len(lines)):
-        current_expression = ''
         result_line = ''  # Строка для хранения результата текущей строки
+        line_contains_operation = False  # Флаг для отслеживания наличия арифметической операции
 
         for char in lines[index]:
             if char.isdigit() or char in '+* ':
                 current_expression += char
+                line_contains_operation = True  # Установить флаг, если есть операция
             else:
                 if current_expression:
                     try:
@@ -70,11 +83,32 @@ def extract_and_evaluate(lines):
                     current_expression = ''
                 result_line += char  # Добавляем текущий символ (буква или знак препинания)
 
-        if current_expression:  # Обработка оставшегося выражения в конце строки
+        if current_expression:
             try:
                 evaluated_result = evaluate_expression(current_expression)
                 result_line += str(evaluated_result)
             except ValueError:
                 result_line += current_expression
 
-        lines[index] = result_line  # Обновляем строку в списке
+        lines[index] = result_line
+
+        if line_contains_operation:
+            lines[index] = lines[index].replace(current_expression, '',
+                                                1).strip()  # Удаляем первую встреченную операцию
+    return lines
+
+
+# Пример использования
+little_prince = [
+    "foo test,",
+    "bar",
+    "foo bar! FOO BAR MAR",
+    "foo baarr, Снег СНЕГ 5 +", "",
+    "5 test, снеговик TEST!!!",
+    "sentence."
+]
+
+# Вызов функции для извлечения и оценки выражений
+result_lines = extract_and_evaluate(little_prince)
+for line in result_lines:
+    print(line)
