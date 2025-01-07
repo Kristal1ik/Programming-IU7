88f85be00131fce24d4efdf19ad6e263976e05b7
kristal1ik <korobovtsevaolga@yandex.ru>
2025-01-07 22:23:19 +0300

    add(exam theory): bubble sort with a flag

---
+++ exam/theory/sort_bubble_flag.py
@@ -0,0 +1,16 @@
+def bubble_sort(array):
+    n = len(array)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n - i - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+                swapped = True
+        if not swapped:
+            break
+    return array
+
+
+array = [64, 34, 25, 12, 22, 11, 90]
+sorted_array = bubble_sort(array)
+print("Отсортированный массив:", sorted_array)
